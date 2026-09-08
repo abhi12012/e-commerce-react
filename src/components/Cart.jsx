0b1deb510aaad0b0c1040a079a8ddfc60a102906
abhi12012 <agrawalabhishek211@gmail.com>
2026-09-08 10:29:58 +0530

    Add empty cart state

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,40 +1,40 @@
 function Cart({ cart, increaseQuantity, decreaseQuantity, removeFromCart }) {
-
-    const total = cart.reduce(
-  (sum, item) => sum + item.product.price * item.quantity,
-  0
-)
+  const total = cart.reduce(
+    (sum, item) => sum + item.product.price * item.quantity,
+    0
+  )
 
   return (
     <div>
       <h2>Cart</h2>
 
-      {cart.map((item) => (
-        <div key={item.product.id}>
-          <p>
-            {item.product.name} × {item.quantity}
-          </p>
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <>
+          {cart.map((item) => (
+            <div key={item.product.id}>
+              <p>
+                {item.product.name} × {item.quantity}
+              </p>
 
-          <button onClick={() => increaseQuantity(item.product.id)}>
-  +
-</button>
+              <button onClick={() => increaseQuantity(item.product.id)}>
+                +
+              </button>
 
-<button onClick={() => decreaseQuantity(item.product.id)}>
-  −
-</button>
+              <button onClick={() => decreaseQuantity(item.product.id)}>
+                −
+              </button>
 
+              <button onClick={() => removeFromCart(item.product.id)}>
+                Remove
+              </button>
+            </div>
+          ))}
 
-
-<button onClick={() => removeFromCart(item.product.id)}>
-  Remove
-</button>
-
-
-        </div>
-      ))}
-
-      <h3>Total: ₹{total}</h3>
-      
+          <h3>Total: ₹{total}</h3>
+        </>
+      )}
     </div>
   )
 }
